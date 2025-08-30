f84dad4a98a90727fbcb64f1d56cfad06e591366
Felipe Vilela <felipe.vilela01@outlook.com>
2025-08-29 21:22:17 -0300

    feat: spinner logic

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/formulario/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/formulario/MainActivity.kt
@@ -1,8 +1,12 @@
 package br.edu.ifsp.scl.ads.prdm.sc303898x.formulario
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc303898x.formulario.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +17,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+
+        activityMainBinding.estadoCivilSp.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val estadpCivilView = (view as TextView).text.toString()
+                val estadoCivil = resources.getStringArray(R.array.estado_civil)[position]
+//                if (estadoCivil.equals("Casado(a)"))
+                if (position == 1){
+                    activityMainBinding.conjugeLl.visibility = View.VISIBLE
+                }else{
+                    activityMainBinding.conjugeLl.visibility = View.GONE
+                    activityMainBinding.nomeConjugeEt.setText("")
+                    activityMainBinding.sobrenomeConjugeEt.setText("")
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
     }
 }
